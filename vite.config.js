6e2d15fe9e74528a1041a8ce70887c80331cf6bf
Suryansh808 <suryanshsaxena808@gmail.com>
2024-12-11 14:19:23 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Krutanic/',
   optimizeDeps: {
     include: ['swiper']
   },
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
